4e5c87bb4dc1d2eb89c62a9efcef0cbe4c172652
Gopal Anand <gopal.anand@sap.com>
2021-03-10 17:10:13 +0530

    series run

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -12,7 +12,7 @@ test("Notifications are loaded", () => {
 
 // Payload for BP Creation, ensure it's unique 
 var payload = {
-    "BusinessPartner": "17100079",
+    "BusinessPartner": "17100082",
     "BusinessPartnerIsBlocked": true,
     "BusinessPartnerFullName": "dark knight"
 };
