bfed63829900373dcf6213fe7533613ea13df57f
Gopal Anand <gopal.anand@sap.com>
2021-03-11 13:54:57 +0530

    Changes to make tests run in order

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -12,7 +12,7 @@ test("Notifications are loaded", () => {
 
 // Payload for BP Creation, ensure it's unique 
 var payload = {
-    "BusinessPartner": "17100089",
+    "BusinessPartner": "17100090",
     "BusinessPartnerIsBlocked": true,
     "BusinessPartnerFullName": "dark knight"
 };
@@ -25,6 +25,58 @@ describe("Validate Notification Status Change", () => {
             token = "Bearer " + bearerToken.data.access_token;
         });
     });
+    // test("Notification recieved in BP", () => {
+    //     let object = { "businessPartnerId": payload.BusinessPartner };
+    //     return loadNotifications.loadNotifications(false, true, payload.BusinessPartner).then(data => {
+    //         console.log("1 ===", data.data);
+    //         testObject.ID = data.data.value[0].ID;
+    //         expect(data.data.value).toMatchObject([object]);
+    //     });
+    // });
+
+    // test(`Notification For Business Partner Creation Recieved`, () => {
+    //     return loadNotifications.loadNotifications(testObject.ID).then(data => {
+    //         console.log("2 ===", data.data);
+    //         expect(data.data).toMatchObject(testObject);
+    //     });
+    // });
+
+    // test("Enable Draft", () => {
+    //     return loadNotificationChange.enableDraft(testObject.ID, token).then(data => {
+    //         expect(data.status).toBe(201);
+    //     });
+    // });
+    // test("Check Stored Draft", () => {
+    //     return loadNotificationChange.checkStoredDraft(testObject.ID, token).then(data => {
+    //         expect(data.status).toBe(200);
+    //     });
+    // });
+    // test("Check Stored Draft Status", () => {
+    //     return loadNotificationChange.checkDraftStatus(testObject.ID, token).then(data => {
+    //         expect(data.status).toBe(200);
+    //     })
+    // });
+    // test("Check Side Effects", () => {
+    //     return loadNotificationChange.checkSideEffects(testObject.ID, token).then(data => {
+    //         expect(data.status).toBe(200);
+    //     })
+    // });
+    // test("Change Notification Status", () => {
+    //     return loadNotificationChange.publishDraft(testObject.ID, token).then(data => {
+    //         expect(data.status).toBe(201);
+    //     })
+    // });
+
+    // test("Notifications Status Change Verification", () => {
+    //     let returnFormat = { "verificationStatus_code": 'V' };
+    //     return loadNotifications.loadNotifications(testObject.ID).then(data => {
+    //         expect(data.data).toMatchObject(returnFormat);
+    //     });
+    // });
+
+});
+
+describe("Notification Recieved in BP", () => {
     test("Notification recieved in BP", () => {
         let object = { "businessPartnerId": payload.BusinessPartner };
         return loadNotifications.loadNotifications(false, true, payload.BusinessPartner).then(data => {
@@ -33,14 +85,17 @@ describe("Validate Notification Status Change", () => {
             expect(data.data.value).toMatchObject([object]);
         });
     });
+});
 
+describe("Notification for BP Creation", () => {
     test(`Notification For Business Partner Creation Recieved`, () => {
         return loadNotifications.loadNotifications(testObject.ID).then(data => {
             console.log("2 ===", data.data);
             expect(data.data).toMatchObject(testObject);
         });
     });
-
+});
+describe("Draft Actions", () => {
     test("Enable Draft", () => {
         return loadNotificationChange.enableDraft(testObject.ID, token).then(data => {
             expect(data.status).toBe(201);
@@ -61,6 +116,8 @@ describe("Validate Notification Status Change", () => {
             expect(data.status).toBe(200);
         })
     });
+});
+describe("Change Status in Service", () => {
     test("Change Notification Status", () => {
         return loadNotificationChange.publishDraft(testObject.ID, token).then(data => {
             expect(data.status).toBe(201);
@@ -73,8 +130,8 @@ describe("Validate Notification Status Change", () => {
             expect(data.data).toMatchObject(returnFormat);
         });
     });
-
 });
+
 
 describe("Change Business Partner Locked Status", () => {
     beforeAll(() => {
