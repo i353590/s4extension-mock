da024f8e00c2e3418d8652d3de88896e95a84235
Gopal Anand <gopal.anand@sap.com>
2021-03-11 15:39:25 +0530

    Varification Status Change

--- tests/jest.test.js
+++ tests/jest.test.js
@@ -12,7 +12,7 @@ test("Notifications are loaded", () => {
 
 // Payload for BP Creation, ensure it's unique 
 var payload = {
-    "BusinessPartner": "17100094",
+    "BusinessPartner": "17100095",
     "BusinessPartnerIsBlocked": true,
     "BusinessPartnerFullName": "dark knight"
 };
@@ -26,73 +26,15 @@ beforeAll(() => {
     });
 });
 describe("Validate Notification Status Change", () => {
-    // beforeAll(() => {
-    //     createNotification.createNotification(payload);
-    //     tokenGenerator.generateToken().then(bearerToken => {
-    //         token = "Bearer " + bearerToken.data.access_token;
-    //     });
-    // });
     test("Notification recieved in BP", () => {
         let object = { "businessPartnerId": payload.BusinessPartner };
         return loadNotifications.loadNotifications(false, true, payload.BusinessPartner).then(data => {
-            console.log("1 ===", data.data);
             testObject.ID = data.data.value[0].ID;
             expect(data.data.value).toMatchObject([object]);
         });
     });
 
-    // test(`Notification For Business Partner Creation Recieved`, () => {
-    //     return loadNotifications.loadNotifications(testObject.ID).then(data => {
-    //         console.log("2 ===", data.data);
-    //         expect(data.data).toMatchObject(testObject);
-    //     });
-    // });
-
-    // test("Enable Draft", () => {
-    //     return loadNotificationChange.enableDraft(testObject.ID, token).then(data => {
-    //         expect(data.status).toBe(201);
-    //     });
-    // });
-    // test("Check Stored Draft", () => {
-    //     return loadNotificationChange.checkStoredDraft(testObject.ID, token).then(data => {
-    //         expect(data.status).toBe(200);
-    //     });
-    // });
-    // test("Check Stored Draft Status", () => {
-    //     return loadNotificationChange.checkDraftStatus(testObject.ID, token).then(data => {
-    //         expect(data.status).toBe(200);
-    //     })
-    // });
-    // test("Check Side Effects", () => {
-    //     return loadNotificationChange.checkSideEffects(testObject.ID, token).then(data => {
-    //         expect(data.status).toBe(200);
-    //     })
-    // });
-    // test("Change Notification Status", () => {
-    //     return loadNotificationChange.publishDraft(testObject.ID, token).then(data => {
-    //         expect(data.status).toBe(201);
-    //     })
-    // });
-
-    // test("Notifications Status Change Verification", () => {
-    //     let returnFormat = { "verificationStatus_code": 'V' };
-    //     return loadNotifications.loadNotifications(testObject.ID).then(data => {
-    //         expect(data.data).toMatchObject(returnFormat);
-    //     });
-    // });
-
 });
-
-// describe("Notification Recieved in BP", () => {
-//     test("Notification recieved in BP", () => {
-//         let object = { "businessPartnerId": payload.BusinessPartner };
-//         return loadNotifications.loadNotifications(false, true, payload.BusinessPartner).then(data => {
-//             console.log("1 ===", data.data);
-//             testObject.ID = (data.data.value.filter(a=>a.verificationStatus_code=="N"))[0].ID;
-//             expect(data.data.value.length >=1).toBeTruthy();
-//         });
-//     });
-// });
 
 describe("Notification for BP Creation", () => {
     test(`Notification For Business Partner Creation Recieved`, () => {
@@ -131,7 +73,7 @@ describe("Change Status in Service", () => {
         })
     });
 
-    test("Notifications Status Change Verification", () => {
+    test("Notifications Status Change to Verified", () => {
         let returnFormat = { "verificationStatus_code": 'V' };
         return loadNotifications.loadNotifications(testObject.ID).then(data => {
             expect(data.data).toMatchObject(returnFormat);
@@ -144,11 +86,12 @@ describe("Change Business Partner Locked Status", () => {
     beforeAll(() => {
         return verifyStatus.mockStatusChangeEvent(payload.BusinessPartner);
     });
-    test("Notifications Status Change Verification", () => {
+    test("Notifications Status Change to Confirmed", () => {
         let returnFormat = { "verificationStatus_code": "C" };
         return loadNotifications.loadNotifications(false, true, payload.BusinessPartner).then(data => {
-            console.log("3 ===", data.data);
-            expect(data.data.value[0]).toMatchObject(returnFormat);
+            console.log("verificationStatus_code", data.data);
+            let temp = data.data.filter((e)=>e.verificationStatus_code === "C");
+            expect(temp.length).toBe(1);
         });
     });
 });
